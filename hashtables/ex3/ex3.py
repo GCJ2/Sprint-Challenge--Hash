9e9403fe7e7792de1e04c26629eb4f6537b906f2
Greg Johnston <gcjohnston91@gmail.com>
2020-05-08 12:28:45 -0400

    ex3 done with comments

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,11 +1,17 @@
 def intersection(arrays):
+	cache = {}
+	returns = []
 
-    """
-    YOUR CODE HERE
-    """
-
-    return result
-
+	for array in arrays:                        # Arrays are nested, so we need to loop through each of them
+		for number in array:                    # For each number in each array
+			if number not in cache:             # If it's not in the cache
+				cache[number] = 1               # Add it with a value of 1
+			elif number in cache:               # If it's already in the cache
+				cache[number] += 1              # Increment it's value by 1
+			if cache[number] == len(arrays):    # If the value of an item in the cache is equal to the number of nested arrays
+				returns.append(number)          # That means it's in all the arrays, so we add it to the list of returns
+	print(returns)
+	return returns
 
 if __name__ == "__main__":
     arrays = []
