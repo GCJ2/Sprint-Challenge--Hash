148d70b4b03465d9c9676514342650ce5321c527
Greg Johnston <gcjohnston91@gmail.com>
2020-05-08 11:37:54 -0400

    EX 2 done. Testing for EX 1 broken for some reason. :(

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,30 @@
+from hashtable import HashTable, HashTableEntry
+import unittest
+
+
 def get_indices_of_item_weights(weights, length, limit):
+	"""
+	Create Hash Table
+	Add each entry to hash table
+	Find higher value
+	If there is a higher value, return it
+	If pair does not exist, return None
+	"""
+	hash_table = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+	for i in range(length):
+		hash_table.put(weights[i], i)
+	for j in range(length):
+		greater = hash_table.get(limit - weights[j])
+		if greater:
+			return greater, j
+		else:
+			return None
 
-    return None
+
+def test_ex1_1():
+	weights_3 = [4, 6, 10, 15, 16]
+	answer_3 = get_indices_of_item_weights(weights_3, 5, 21)
+	print(f'answer_3 {answer_3}')
+
+test_ex1_1()
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,14 +1,32 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
+from hashtable import HashTable
+
+
 class Ticket:
-    def __init__(self, source, destination):
-        self.source = source
-        self.destination = destination
+	def __init__(self, source, destination):
+		self.source = source
+		self.destination = destination
 
 
 def reconstruct_trip(tickets, length):
-    
-    """
-    YOUR CODE HERE
-    """
+	"""
+	YOUR CODE HERE
+	"""
+	ht = HashTable(length)
+	route = [None] * length
 
-    return route
+	for i in range(len(tickets)):
+		if tickets[i].source == 'NONE':
+			beginning = tickets[i].destination
+		ht.put(tickets[i].source, tickets[i].destination)
+
+	route = []
+
+	for i in range(len(tickets)):
+		if i == 0:
+			route.append(beginning)
+		else:
+			ticket = ht.get(route[i-1])
+			route.append(ticket)
+
+	return route
